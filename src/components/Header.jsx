b124c1bb9805cede3a2000e4f03da84198cf415b
Ghassaan <ghassaank1@gmail.com>
2024-06-05 14:38:45 +0500

    Header finalized

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Header.css'
 import logo from '../assets/logo.png'
+import msg from '../assets/VectorMessage.png'
+import profile from '../assets/VectorProfile.png'
+import orders from '../assets/Vectororders.png'
+import cart from '../assets/Vectorcart.png'
 
 const Header = () => {
   return (
@@ -11,7 +15,24 @@ const Header = () => {
           <input type="text" id="searchItem" placeholder='Search' />
           <input type="button" className='btn' value="Search" />
         </div>
-        <div className="mainmenu"></div>
+        <div className="mainmenu">
+          <div className="sect">
+          <img src={profile} alt="Profile"/>
+            Profile
+          </div>
+          <div className="sect">
+          <img src={msg} alt="Messages"/>
+            Messages
+          </div>
+          <div className="sect">
+          <img src={orders} alt="Orders"/>
+            Orders
+          </div>
+          <div className="sect">
+          <img src={cart} alt="My Cart"/>
+            My Cart
+          </div>
+        </div>
       </div>
     </div>
   )
